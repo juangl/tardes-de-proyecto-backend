4041c301460e4f6c120e752eb280c9c72beea491
Juan Je Garcia <juanjegarcialopez@gmail.com>
2021-03-30 20:08:36 -0600

    first routes

--- app.js
+++ app.js
@@ -1,11 +1,35 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send('<h2>hola</h2>');
+const posts = [
+  {
+    title: "first post",
+    created_at: new Date(),
+    body: "here's the content",
+  },
+];
+
+// main route
+app.get("/posts", (req, res) => {
+
+  res.json(posts);
+
 });
 
+app.get("/posts/create", (req, res) => {
+  const newPost = {
+    title: "second post",
+    created_at: new Date(),
+    body: "here's the content",
+  };
+
+  posts.push(newPost);
+  
+  res.json(newPost);
+});
+
+// start our server
 app.listen(port, () => {
-    console.log(`server running in http://localhost:${port}`);
-})
+  console.log(`server running in http://localhost:${port}`);
+});
