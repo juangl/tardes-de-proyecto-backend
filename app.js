83a08894316de8d85e855127d79dcdbf752ac7d5
Sebastian <sebgogz@gmail.com>
2021-04-09 19:04:09 -0500

    users

--- app.js
+++ app.js
@@ -63,6 +63,51 @@ app.delete("/posts/:postId", async (req, res) => {
   }
 });
 
+app.post("/register", async (req, res) => {
+  try {
+    const user = await prisma.user.create({
+      data: {
+        username: req.body.username,
+        password: req.body.password,
+      },
+    });
+    res.json(user);
+  } catch (e) {
+    if (e.code === 'P2002') {
+      res.json({ message: 'El nombre de usuario ya existe' });
+    } else {
+      res.status(500).json({ message: 'Error' });
+    }
+  }
+});
+
+app.get("/users", async (req, res) => {
+  try {
+    const users = await prisma.user.findMany();
+    res.json(users);
+  } catch (e) {
+    res.status(500).json({ message: 'Error' });
+  }
+});
+
+app.post("/login", async (req, res) => {
+  try {
+    const user = await prisma.user.findFirst({
+      where: {
+        username: req.body.username,
+        password: req.body.password,
+      },
+    });
+    if (user) {
+      res.json({ message: 'El usuario se loggeo correctamente' });
+    } else {
+      res.status(404).json({ message: 'El usuario o la contraseña son incorrectas' });
+    }
+  } catch (e) {
+    res.status(500).json({ message: 'Error' });
+  }
+});
+
 
 // collection of posts
 /*
